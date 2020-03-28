1305a4354893dab5e86b8666872f796aa2ae2106
JudgeFudge19 <dremukare@gmail.com>
2020-03-28 14:49:52 +0300

    finished adding functionality and tweaking styling

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 // Define ui variables 
-const form = document.querySelector('.user-form');
+const form = document.querySelector('.form');
 const numInput = document.querySelector('#numInput');
-const btn = document.querySelector('.btn');
-const output = document.querySelector('.output');
+const finalOutputDiv = document.querySelector('#finalOutput');
 
 // Function to load event listener
 loadEventListeners();
@@ -10,20 +9,43 @@ loadEventListeners();
 // Defining function to load event listeners
 function loadEventListeners(){
   // Adding event listener to form
-  document.addEventListener('submit', compute);
+  form.addEventListener('submit', compute);
   // Adding event listener to numInput
+  numInput.addEventListener('keyup', onInput);
   // Adding event listener to btn
   // Adding event listener to output
 }
 
+// Function to clear out final output div on start of new user input
+function onInput() {
+  finalOutputDiv.innerHTML = '';
+}
+
 // Function for form validation and number conversion
 function compute(e) {
-  if(numInput.value.length > 8) {
-    alert('Maximum number of digits allowed is 8!');
-  }
   if(numInput.value === '') {
     alert('Input number first!');
-  } // Form validation does not currently work. Figure out why!
+  } else{
+    // Converting string that user enters into a number
+    var binary = parseInt(numInput.value);
+    // Converting entered binary number into decimal numbersystem
+    var answer = parseInt(binary, 2);
+  
+    // Creating p element
+    const p = document.createElement('p');
+    // Assigning class to p 
+    p.className = 'output lead w-100 pt-3';
+    // Adding styling (this is essentially bad practice i think)
+    p.style.marginTop = '7px';
+    // Adding text node to contain answer
+    p.appendChild(document.createTextNode(answer)) 
+    // Appending p to parent div
+    finalOutputDiv.appendChild(p);
+  }
+  if(numInput.value.length > 8) {
+    alert('The maximum number of digits you can enter is 8');
+  }
+
 
   // Clear input field on submit
   numInput.value = '';
